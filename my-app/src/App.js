7e7b9722d1b4c7981204370e65bee5a3bcf9fdeb
Sarulaaa <saruulsaruul771@gmail.com>
2023-04-08 17:07:21 +0800

    test5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       hi github  
-=======
+
       hi yu bna
->>>>>>> fe367eb8f30e9173d9ba4edc48410fdf41a879d4
+
+      ene dahaid ooorchlolt
+
     </div>
   );
 }
